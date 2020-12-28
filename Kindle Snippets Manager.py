67b988f4b0db4337194e73689ce8ae5bec61487e
James Gourley <jcgourl1@asu.edu>
2020-12-28 16:35:16 -0700

    Added File Uploader

--- Kindle Snippets Manager.py
+++ Kindle Snippets Manager.py
@@ -4,18 +4,35 @@ import numpy as np
 import seaborn as sns
 
 import sys
+import io
 import streamlit as st
 
-pd.set_option('display.max_rows', 500)
+# File uploader
+uploaded_file = st.file_uploader("Upload File Here")
+
+# Create Pandas DataFrame from myclippings.txt file
+#def SnippetDataFrame(uploaded_file):
+    # Create a list item for every line in myclippings.txt
 
 # Create a list item for every line in myclippings.txt
 clippings_list = []
 
-with open('My Clippings - Copy.txt', 'r+', encoding="utf8") as file:
-    for line in file:
-        line = line.rstrip('\n')
-        line = line.replace('\ufeff', '')
-        clippings_list.append(line)
+if uploaded_file is not None:
+    for line in uploaded_file:
+        #line = line.replace('\n', '')
+        #line = line.replace('\ufeff', '')
+        #clippings_list.append(line)
+        st.write(line)
+
+# LEFT OFF HERE - still trying to figure out how to read line by line when the uploaded file is read in bytes!!
+
+# with open('My Clippings - Copy.txt', 'r+', encoding="utf8") as file:
+#     for line in file:
+#         line = line.rstrip('\n')
+#         line = line.replace('\ufeff', '')
+#         clippings_list.append(line)
+
+
 
 # Remove empty lines from list
 for item in clippings_list:
@@ -23,6 +40,7 @@ for item in clippings_list:
         clippings_list.remove(item)
     else:
         pass
+
 
 # Re-package into list of lists for each item ending at separator "=========="
 clippings_list_final = []
@@ -55,5 +73,4 @@ clippings_df = clippings_df.drop_duplicates(subset='Clipping', keep="first")
 clippings_df[['Book Title', 'Author']] = clippings_df['Book Title'].str.split('(', expand=True)
 clippings_df['Author'] = clippings_df['Author'].str.replace(')', '')
 
-clippings_df.head()
 
