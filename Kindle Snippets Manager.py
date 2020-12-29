3ca478549fee3fcc6d172015427fbd9ec2347b4d
James Gourley <jcgourl1@asu.edu>
2020-12-29 13:46:51 -0700

    Data Frame method for parsing myclippings.txt file

--- Kindle Snippets Manager.py
+++ Kindle Snippets Manager.py
@@ -10,77 +10,48 @@ import streamlit as st
 # File uploader
 uploaded_file = st.file_uploader("Upload File Here")
 
-# Create Pandas DataFrame from myclippings.txt file
-#def SnippetDataFrame(uploaded_file):
-    # Create a list item for every line in myclippings.txt
+# Read uploaded file to a dataframe
+clippings_df = pd.read_csv(uploaded_file, header=None, sep='\n')
 
-# Convert uploaded file into a string and do some cleanup
-uploaded_file_str = str(uploaded_file.read(), 'utf-8')
-#print(uploaded_file_str)
-
-# Convert to DF
-uploaded_file_str = io.StringIO(uploaded_file_str)
-df = pd.read_csv(uploaded_file, sep="==========")
-print(df)
-# Convert uploaded file into a list
-uploaded_file_list = list(uploaded_file_str.split(" "))
-
-# Create a list item for every line in myclippings.txt
-clippings_list = []
-
-#if uploaded_file is not None:
-    #for line in uploaded_file_str:
-        #line = line.replace('\n', '')
-        #line = line.replace('\ufeff', '')
-        #clippings_list.append(line)
-        #st.write(line)
-
-
-# with open('My Clippings - Copy.txt', 'r+', encoding="utf8") as file:
-#     for line in file:
-#         line = line.rstrip('\n')
-#         line = line.replace('\ufeff', '')
-#         clippings_list.append(line)
-
-
-
-# Remove empty lines from list
-for item in clippings_list:
-    if item == '':
-        clippings_list.remove(item)
-    else:
-        pass
-
-
-# Re-package into list of lists for each item ending at separator "=========="
+# Turn df into list of lists based on snippet groups for easier transformation into clean data frame
 clippings_list_final = []
 temp_list = []
 
-for item in clippings_list:
-    if item == '==========':
+for index, row in clippings_df[0].items():
+    if row == '==========':
         clippings_list_final.append(temp_list)
         temp_list = []
     else:
-        temp_list.append(item)
+        temp_list.append(row)
 
-# clippings_list_final
-
-# Create a data frame from the list
+# Create a new data frame from the list of lists
 clippings_df = pd.DataFrame(clippings_list_final)
 
 # Merge clippings that flow onto multiple lines into one column
-clippings_df['Clipping'] = clippings_df[clippings_df.columns[2:]].apply(lambda x: ' '.join(x.dropna().astype(str)),
-                                                                        axis=1)
+clippings_df['Clipping'] = clippings_df[clippings_df.columns[2:]].apply(lambda x: ' '.join(x.dropna().astype(str)), axis=1)
 
 # Remove the extra columns & rename
 clippings_df = clippings_df[[0, 1, 'Clipping']]
 clippings_df.columns = ['Book Title', 'Metadata', 'Clipping']
 
-# Remove duplicate clippings - sometimes happens due to Kindle glitch
-clippings_df = clippings_df.drop_duplicates(subset='Clipping', keep="first")
-
 # Expand the Book Title & Metadata info into individual columns
-clippings_df[['Book Title', 'Author']] = clippings_df['Book Title'].str.split('(', expand=True)
+clippings_df[['Book Title','Author']] = clippings_df['Book Title'].str.split('(', expand=True)
 clippings_df['Author'] = clippings_df['Author'].str.replace(')', '')
+
+# Derive a Location column & Page column (if applicable)
+clippings_df['Location'] = clippings_df['Metadata'].str.extract(r'(Location \d+)')
+clippings_df['Page'] = clippings_df['Metadata'].str.extract(r'(page \d+)')
+clippings_df['Date Added'] = clippings_df['Metadata'].str.extract(r'(Added on .+)')
+clippings_df['Date Added'] = clippings_df['Date Added'].str.replace('Added on ', '')
+# Still need to turn Date Added column into a date time
+
+# Remove duplicate clippings - sometimes happens due to Kindle glitch
+print(len(clippings_df))
+clippings_df = clippings_df.drop_duplicates(subset='Clipping', keep="last")
+print(len(clippings_df))
+clippings_df = clippings_df.drop_duplicates(subset=['Location', 'Author'], keep="last")
+print(len(clippings_df))
+
+st.write(clippings_df)
 
 
