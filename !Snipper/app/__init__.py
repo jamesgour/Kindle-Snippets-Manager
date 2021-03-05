258fe0e17bb5b6672ff11e8c32573ac2c248b6b3
James Gourley <jcgourl1@asu.edu>
2021-03-05 08:59:34 -0700

    Started Adding Database

--- !Snipper/app/__init__.py
+++ !Snipper/app/__init__.py
@@ -1,16 +1,23 @@
-from flask import Flask, url_for
+from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
 
 
 # Register Extensions
+db = SQLAlchemy()
+migrate = Migrate()
 bootstrap = Bootstrap()
+
 
 def create_app(test_config=None):
     app = Flask(__name__)
     app.config.from_object(Config)
 
     # Init Extensions
+    db.init_app(app)
+    migrate.init_app(app, db)
     bootstrap.init_app(app)
 
     # Blueprint Imports
@@ -24,3 +31,6 @@ def create_app(test_config=None):
     app.register_blueprint(snippetimport.snippetimport_bp)
 
     return app
+
+
+from app import models
--- !Snipper/app/authentication/routes.py
+++ !Snipper/app/authentication/routes.py
@@ -8,5 +8,5 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/')
+        return redirect('/dailybriefing')
     return render_template('login.html', title='Login', form=form)
